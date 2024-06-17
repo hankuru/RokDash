e66c599da4531c3df049d135a1a350198ac9866a
hankuru <hankurushadow123@hotmail.com>
2024-06-17 12:05:45 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2808,7 +2808,7 @@ if selected == "Contact":
         st.write("")
         st.subheader(":mailbox: Any Question? Ask Away 🙌")
         contact_form = f"""
-        <form action="https://formsubmit.co/{"usman.mustafa88@gmail.com"}" method="POST">
+        <form action=""}" method="POST">
             <input type="hidden" name="_captcha" value="false">
             <input type="text" name="name" placeholder="Your name" required>
             <input type="email" name="email" placeholder="Your email" required>
